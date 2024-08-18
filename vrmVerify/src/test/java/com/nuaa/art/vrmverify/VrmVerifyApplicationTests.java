986e5474d42413bef5402d0384b107474c8e2f4b
blue <749226313@qq.com>
2024-08-18 22:15:53 +0800

    为CTLParseUtils编写测试用例

--- vrmVerify/src/test/java/com/nuaa/art/vrmverify/VrmVerifyApplicationTests.java
+++ vrmVerify/src/test/java/com/nuaa/art/vrmverify/VrmVerifyApplicationTests.java
@@ -1,13 +1,21 @@
 package com.nuaa.art.vrmverify;
 
+import com.nuaa.art.vrmverify.common.exception.InvalidCTLException;
+import com.nuaa.art.vrmverify.common.utils.CTLParseUtils;
+import com.nuaa.art.vrmverify.model.formula.ctl.CTLFormula;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = VrmVerifyApplicationTests.class)
 class VrmVerifyApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void testParseCTL() {
+        CTLFormula correctCTL1 = CTLParseUtils.parseCTLStr("AG(wolf = TRUE)");
+        CTLFormula correctCTL2 = CTLParseUtils.parseCTLStr("EF(wolf = TRUE)");
+        CTLFormula correctCTL3 = CTLParseUtils.parseCTLStr("AX(wolf = TRUE)");
+        CTLFormula correctCTL4 = CTLParseUtils.parseCTLStr("E[wolf = TRUE U sheep = TRUE]");
+        CTLFormula incorrectCTL = CTLParseUtils.parseCTLStr("G(wolf = TRUE)");
     }
 
 }
